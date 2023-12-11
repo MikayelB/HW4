2fd720f08df0389b664d6c04c3ce61500abf0295
Mikayel <mikael.bejanyan@gmail.com>
2023-12-11 03:48:03 +0100

    Added settings button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-    packaging {
+    packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-
     implementation("com.google.android.engage:engage-core:1.3.1")
     val navVersion = "2.7.5"
 
@@ -69,31 +68,24 @@ dependencies {
     androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
 
     // Jetpack Compose Integration
-    implementation("androidx.navigation:navigation-compose:$navVersion")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation ("androidx.navigation:navigation-compose:2.7.5")
-
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.ui:ui-graphics:1.0.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.0.0")
+    implementation("androidx.compose.material3:material3:1.0.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.runtime:runtime:1.0.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.0.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.0")
 }
--- app/src/main/java/com/example/cities_mobile_dubl2/ui/WelcomeScreen.kt
+++ app/src/main/java/com/example/cities_mobile_dubl2/ui/WelcomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.cities_mobile_dubl2.ui
 
 import android.content.Context
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,10 +10,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -24,17 +35,45 @@ import androidx.navigation.NavController
 import com.example.cities_mobile_dubl2.MainActivity
 import com.example.cities_mobile_dubl2.R
 import com.example.cities_mobile_dubl2.constants.SECOND_SCREEN_ROUTE
-import com.example.cities_mobile_dubl2.network.WeatherService
+import com.example.cities_mobile_dubl2.model.WeatherData
 import com.example.cities_mobile_dubl2.viewmodel.WeatherViewModel
-import kotlinx.coroutines.launch
-import android.location.LocationListener
 import kotlinx.coroutines.delay
-
+import kotlinx.coroutines.launch
 
 @Composable
 fun WelcomeScreen(navController: NavController, activity: MainActivity, viewModel: WeatherViewModel) {
     val context = LocalContext.current
 
+    var weatherData by remember { mutableStateOf<List<WeatherData>?>(null) }
+    var isDataLoaded by remember { mutableStateOf(false) }
+
+    LaunchedEffect(weatherData) {
+        if (weatherData != null && weatherData!!.isNotEmpty() && weatherData!![0].location.name.isNotEmpty()) {
+            isDataLoaded = true
+        }
+    }
+
+    viewModel.weatherData.observeAsState().value?.let {
+        weatherData = it
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp, 70.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Button(
+            onClick = {
+                navController.navigateUp()
+            },
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.Start),
+
+            ) {
+            Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -49,16 +88,16 @@ fun WelcomeScreen(navController: NavController, activity: MainActivity, viewMode
         )
 
         if (activity.checkLocationPermission()) {
-            val weatherData = viewModel.weatherData.value
-            if (weatherData != null) {
+            if (isDataLoaded) {
+                val cityName = weatherData?.get(0)?.location?.name ?: ""
                 Text(
-                    text = "Current Temperature: ${weatherData[0].current.temp_c}°C",
+                    text = "Current Temperature in $cityName: ${weatherData?.get(0)?.current?.temp_c}°C",
                     fontSize = 16.sp,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
             } else {
                 Text(
-                    text = "No weather available",
+                    text = "Loading...",
                     fontSize = 16.sp,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
@@ -85,6 +124,7 @@ fun WelcomeScreen(navController: NavController, activity: MainActivity, viewMode
         }
     }
 }
+
 
 private fun fetchWeatherForCurrentLocation(activity: MainActivity, viewModel: WeatherViewModel) {
     val locationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
